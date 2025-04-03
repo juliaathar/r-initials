d483ab50783436447ed11c7b57f4bef9bf75f9d2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-03 19:36:13 -0300

    add functions

--- r_initials.R
+++ r_initials.R
@@ -1,12 +1,3 @@
-idade =  20
-tem_carteira = TRUE
-
- if(idade >=  18 &&  tem_carteira==TRUE){
-   print('pode dirigir')
- } else{
-   print('não pode dirigir')
- }
-
 #function | b.h/2
 
 area <- function(b,h){
@@ -14,3 +5,27 @@ area <- function(b,h){
 }
 
 area(21,32)
+
+#function | pi * r^2
+
+area_circunferencia <- function(raio){
+  return(pi*(raio^2))
+}
+
+area_circunferencia(10)
+
+#function | 2(b.h)
+
+area_perimetro <- function( b,h){
+  return(2*(b+h))
+}
+
+area_perimetro(12,4)
+
+#function | a(B + b)*h/2
+
+area_trapezio <- function(b.maior, b.menor, h){
+  return(((b.maior + b.menor) *h)/2)
+}
+
+area_trapezio(5, 3, 4)
