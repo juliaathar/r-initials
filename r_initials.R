0b7f65789066cd501c0d93f1c74481a412996fd8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-03 19:16:38 -0300

    add function

---
+++ r_initials.R
@@ -0,0 +1,16 @@
+idade =  20
+tem_carteira = TRUE
+
+ if(idade >=  18 &&  tem_carteira==TRUE){
+   print('pode dirigir')
+ } else{
+   print('não pode dirigir')
+ }
+
+#function | b.h/2
+
+area <- function(b,h){
+  return(b*h/2)
+}
+
+area(21,32)
