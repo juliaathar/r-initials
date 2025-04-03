a62543458b562d3ac9cead9ce2c5ddb52df90d72
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-03 20:55:41 -0300

    add type of data

--- r_initials.R
+++ r_initials.R
@@ -29,3 +29,52 @@ area_trapezio <- function(b.maior, b.menor, h){
 }
 
 area_trapezio(5, 3, 4)
+
+
+#-----------------------------------objects--------------------------------------
+
+#vectors
+vector1 = 1:7 #para números que vão sequencialmente
+vector1
+
+vector2 = c(1,4,2,5,7,3,8,9) #para números específicos | concatenate
+vector2
+
+vector3 = c("Ana", "Paulo", " Ricardo", "Gabriela")
+vector3
+
+vector3[1]
+
+
+# matrix
+M1 = matrix(1:8, nrow = 4, ncol = 2)
+M1
+
+M2 = matrix(c(1,4,2,7,3,9,5,3),
+            nrow = 4,
+            ncol=2,
+            dimnames = list(c("L1","L2",'L3', 'L4'), c("C1", "C2"))
+            )
+M2
+
+#array
+numeros = 1:12
+
+meus_numeros =array(numeros, dim= c(2,3,2))
+meus_numeros
+
+# data frame
+tabela =  data.frame(Nome=c('Júlia', "Bruno", "Edgar", "Fabiano", "Igor"),
+                     Idade= c(20, 21, 17, 18,17),
+                     Peso = c(65,72,27,85,90),
+                     Altura = c(1.60, 1.75,1.50,1.30,1.60),
+                     Cidade = c("Santo André", "São Paulo", "São Caetano", "São Paulo", "Embu das Artes")
+                     )
+tabela
+
+ #lista
+lista_objetos = list(vetor_numerico= c(1.5,2.3,4.8),
+                     vetor_texto = c("Júlia", "Bruno", "Edgar") 
+                     )
+
+lista_objetos
